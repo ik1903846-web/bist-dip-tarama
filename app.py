b4313b2e845762d27b3e49f99e7ee764c1324fa1
ik1903846-web <ik1903846@gmail.com>
2026-04-05 01:15:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,2 @@
 import streamlit as st
-
-st.set_page_config(page_title="BIST Tarayıcı", page_icon="📈")
-st.title("📈 BIST Tarayıcı")
-st.info("Sol menüden 'yenisistem' sayfasına geçin.")
+st.switch_page("pages/yenisistem.py")
