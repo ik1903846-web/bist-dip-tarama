31abc60a8f0a1fefb5380f84a26a19a08bd3999b
ik1903846-web <ik1903846@gmail.com>
2026-04-05 03:22:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
 import streamlit as st
-import subprocess
-import sys
-
-st.set_page_config(page_title="BIST Tarayıcı", page_icon="📈")
-
-pg = st.navigation([
-    st.Page("pages/yenisistem.py", title="BIST Tarayıcı", icon="📈"),
-])
-pg.run()
+st.title("Test")
+st.write("Çalışıyor!")
